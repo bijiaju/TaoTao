b5b692a2f914c410b4e615ef192f2279fd7f8c44
bijiaju <247636225@qq.com>
2018-10-31 22:02:18 +0800

    测试配置跑通了

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemService.java
@@ -0,0 +1,10 @@
+package com.taotao.service;
+
+import com.taotao.pojo.TbItem;
+
+public interface ItemService {
+
+	TbItem getItemById(long itemId);
+	//EUDataGridResult getItemList(int page, int rows);
+	//TaotaoResult createItem(TbItem item, String desc, String itemParam) throws Exception;
+}
